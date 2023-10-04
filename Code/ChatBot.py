1964d00b6319b4bb7d35d9785f1b01135982f8c2
iSiddharth20 <kekresiddharth20@gmail.com>
2023-10-03 20:51:04 -0700

    Codebase for ChatBot

---
+++ Code/ChatBot.py
@@ -0,0 +1,25 @@
+# Importing Bot Defination
+from BotDefinition import OpenAIBot
+
+'''Choose whichever model you want to use'''
+# engine = "gpt-3.5-turbo"
+# engine = "gpt-3.5-turbo-16k"
+engine = "gpt-4"
+
+'''
+Creating the ChatBot
+Each Object of "OpenAIBot(engine)" will retain the conversation history and context unless the session is terminated
+'''
+chatbot = OpenAIBot(engine)
+
+while True:
+    # Get Prompt from User
+    prompt = input()
+
+    # User can stop the chat by sending 'End Chat' as a Prompt
+    if prompt.lower() == 'END CHAT':
+        break
+
+    # Generate and Print the Response from ChatBot
+    response = chatbot.generate_response(prompt)
+    print(response)
